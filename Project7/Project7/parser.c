7fad1701c2a28c93051ba6964df038a4885192a4
Dorado9 <65004308+Dorado9@users.noreply.github.com>
2021-12-19 15:06:37 +0200

    Delete compailer.c

--- Project7/Project7/parser.c
+++ Project7/Project7/parser.c
@@ -9,6 +9,8 @@ int hex2dec(char hex[]) {
 			dec += (hex[i] - 48) * base;
         else if (hex[i] >= 'A' && hex[i] <= 'F')
 			dec += (hex[i] - 55) * base;
+		else if (hex[i] >= 'a' && hex[i] <= 'f')
+			dec += (hex[i] - 'a' + 10) * base;
         base *= 16;
     }
     return dec;
@@ -20,8 +22,9 @@ void parse_cmd(char *line) {
 
 void parse_asm(char *fname) {
 	FILE *fp;
-	char line[MAX_STRLEN], str[MAX_LABEL], *;
+	char line[MAX_STRLEN], str[MAX_LABEL];
 	label x = {0, 0};
+	int line_count = 0;
 
 	fp = fopen(fname, "r");
 	while (fgets(line, MAX_STRLEN, fp)) {
@@ -30,11 +33,14 @@ void parse_asm(char *fname) {
 		if (strcmp(line, str))	//if it's a command
 			continue;
 		else {					//if it's a label
-
+			strcpy(x.name, str);
+			x.address = line_count;
 		}
+		line_count++;
 	}
 
 	rewind(fp);
+	line_count = 0;
 
 	while (fgets(line, MAX_STRLEN, fp)) {
 		str = strtok(line, ":");
