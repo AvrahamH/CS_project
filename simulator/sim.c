057e2975cbae4a716f6179363fa9a0c40e4d333c
--global <avrahamhrinevitzky@Avrahams-MacBook-Air.local>
2021-12-30 16:10:19 +0200

    new function

--- simulator/sim.c
+++ simulator/sim.c
@@ -3,6 +3,180 @@
 
 #define TO_SIGNED(num) ((num & 0xFFF) | ((num & 0x800) ? ~0xFFF : 0))
 
+void cmd_regs(cmd lines[MAX_MEM_LEN], uint32_t hw_regs[HW_COUNT],int regs[REG_COUNT],const * argv[]){
+	int pc = 0; cmd_line = 0;
+	// now we are going through the command lines and change the registers according to the command 
+	while(line[cmd_Line].name != 21){
+		write_trace(line[cmd_line],pc,argv[TRACE],regs); // every command we wanr to update the trace
+		// chacking if there is a illegal change to a ZERO or IMM registers  
+		if(lines[cmd_line].rd  == ZERO | lines[cmd_line].rd  == IMM1 | lines[cmd_line].rd  == IMM2  ){
+			pc ++;
+			hw_regs[CLKS]++;
+			cmd_line ++;
+			continue;
+			}
+		// else we change the imm registers if we use them in the current command and  implement the command according to the operation 
+		else{
+			if((lines[cmd_line].rs == IMM1 | lines[cmd_line].rs == IMM2)| (lines[cmd_line].rt == IMM1 | lines[cmd_line].rt == IMM2) | (lines[cmd_line].rm == IMM1 | lines[cmd_line].rm == IMM2)){
+				if(lines[cmd_line].rs == IMM1 | lines[cmd_line].rs == IMM2){
+					if(lines[cmd_line].rs == IMM1)
+						lines[cmd_line].rs == lines[cmd_line].imm1;
+					else
+						lines[cmd_line].rs == lines[cmd_line].imm2;	
+				}	
+				if(lines[cmd_line].rt == IMM1 | lines[cmd_line].rt == IMM2){
+					if(lines[cmd_line].rt == IMM1)
+						lines[cmd_line].rt == lines[cmd_line].imm1;
+					else
+						lines[cmd_line].rt == lines[cmd_line].imm2;			
+				}
+				if(lines[cmd_line].rm == IMM1 | lines[cmd_line].rm == IMM2){
+					if(lines[cmd_line].rm == IMM1)
+						lines[cmd_line].rm == lines[cmd_line].imm1;
+					else
+						lines[cmd_line].rm == lines[cmd_line].imm2;			
+				}
+			}
+			switch(lines[cmd_line].name){
+				case 0 :
+					regs[lines[cmd_line].rd] = regs[cmd_line.rs] + regs[cmd_line.rt] + regs[cmd_line.rm];
+					pc ++;
+					hw_regs[CLKS]++;
+					cmd_line ++;
+					break;
+				case 1 :
+					regs[lines[cmd_line].rd] = regs[lines[cmd_line].rs] - regs[lines[cmd_line].rt] - regs[lines[cmd_line].rm];
+					pc ++;
+					hw_regs[CLKS]++;
+					cmd_line ++;
+					break;
+				case 2:
+					regs[lines[cmd_line].rd] = regs[lines[cmd_line].rs] * regs[lines[cmd_line].rt] - regs[lines[cmd_line].rm];
+					pc ++;
+					hw_regs[CLKS]++;
+					cmd_line ++;
+					break;
+				case 3 :
+					regs[lines[cmd_line].rd] = regs[lines[cmd_line].rs] & regs[lines[cmd_line].rt] & regs[lines[cmd_line].rm];
+					pc ++;
+					hw_regs[CLKS]++;
+					cmd_line ++;
+					break;
+				case 4:
+					regs[lines[cmd_line].rd] = regs[lines[cmd_line].rs] | regs[lines[cmd_line].rt] | regs[lines[cmd_line].rm];
+					pc ++;
+					hw_regs[CLKS]++;
+					cmd_line ++;
+					break;
+				case 5 :
+					regs[lines[cmd_line].rd] = regs[lines[cmd_line].rs] ^ regs[lines[cmd_line].rt] ^ regs[lines[cmd_line].rm];
+					pc ++;
+					hw_regs[CLKS]++;
+					cmd_line ++;
+					break;
+				case 6:
+					regs[lines[cmd_line].rd] = regs[lines[cmd_line].rs] << regs[lines[cmd_line].rt] ;
+					pc ++;
+					hw_regs[CLKS]++;
+					cmd_line ++;
+					break;
+				case 7 :
+					//	NEED TO DO ARITH SHIFT WITH SIGN EXTANSION 
+					break;
+				case 8:
+					regs[lines[cmd_line].rd] = regs[lines[cmd_line].rs] >> regs[lines[cmd_line].rt] ;
+					pc ++;
+					hw_regs[CLKS]++;
+					cmd_line ++;
+					break;
+				case 9 :
+					if(regs[lines[cmd_line].rs] == regs[lines[cmd_line].rt]){
+						pc = regs[lines[cmd_line].rm];
+						hw_regs[CLKS]++;
+						cmd_line = regs[lines[cmd_line].rm];
+						break;
+					}
+					pc ++;
+					hw_regs[CLKS]++;
+					cmd_line ++;
+					break;
+				case 10:
+					if(regs[lines[cmd_line].rs] != regs[lines[cmd_line].rt]){
+						pc = regs[lines[cmd_line].rm];
+						hw_regs[CLKS]++;
+						cmd_line = regs[lines[cmd_line].rm];
+						break;
+					}
+					pc ++;
+					hw_regs[CLKS]++;
+					cmd_line ++;
+					break;
+				case 11 :
+					if(regs[lines[cmd_line].rs] < regs[lines[cmd_line].rt]){
+						pc = regs[lines[cmd_line].rm];
+						hw_regs[CLKS]++;
+						cmd_line = regs[lines[cmd_line].rm];
+						break;
+					}
+					pc ++;
+					hw_regs[CLKS]++;
+					cmd_line ++;
+					break;
+				case 12:
+					if(regs[lines[cmd_line].rs] > regs[lines[cmd_line].rt]){
+						pc = regs[lines[cmd_line].rm];
+						hw_regs[CLKS]++;
+						cmd_line = regs[lines[cmd_line].rm];
+						break;
+					}
+					pc ++;
+					hw_regs[CLKS]++;
+					cmd_line ++;
+					break;
+				case 13 :
+					if(regs[lines[cmd_line].rs] <= regs[lines[cmd_line].rt]){
+						pc = regs[lines[cmd_line].rm];
+						hw_regs[CLKS]++;
+						cmd_line = regs[lines[cmd_line].rm];
+						break;
+					}
+					pc ++;
+					hw_regs[CLKS]++;
+					cmd_line ++;
+					break;
+				case 14:
+					if(regs[lines[cmd_line].rs] >= regs[lines[cmd_line].rt]){
+						pc = regs[lines[cmd_line].rm];
+						hw_regs[CLKS]++;
+						cmd_line = regs[lines[cmd_line].rm];
+						break;
+					}
+					pc ++;
+					hw_regs[CLKS]++;
+					cmd_line ++;
+					break;
+				case 15 :
+					regs[lines[cmd_line].rd] = pc + 1;
+					pc = regs[lines[cmd_line].rm];
+					hw_regs[CLKS]++;
+					cmd_line = regs[lines[cmd_line].rm];
+					break;
+				case 16:
+				break;
+				case 17 :
+				break;
+				case 18:
+				break;
+				case 19 :
+				break;
+				case 20:
+				break;
+				}
+			}
+		}
+	}
+
+
 int hex2dec(char hex[]) {
 	int len = strlen(hex), base = 1, dec = 0;
 	for (int i = len - 1; i >= 0; i--) {
